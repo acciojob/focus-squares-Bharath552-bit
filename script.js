f90c778e6eebdc8c31b11fa8ca9b2311d0e46a9d
Bharath552-bit <patthembharath32@gmail.com>
2026-01-11 20:40:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+	const squares=document.querySelectorAll(".square")
+	squares.forEach((hoveredsquare)=>{
+		hoveredsquare.addEventListener('mouseenter',()=>{
+			squares.forEach(square=>{
+				if(square!==hoveredsquare){
+					square.style.backgroundColor="#6F4E37"
+				}
+			})
+		})
+		hoveredsquare.addEventListener('mouseleave',()=>{
+			squares.forEach(square=>{
+					square.style.backgroundColor="#E6E6FA"
+			})
+		})
+		
+	})
+	
